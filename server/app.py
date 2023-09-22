b9b74ac3cd4e76ef398bf263d014fdc9e9bcbc16
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-09-22 13:34:48 +0300

    Partial

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
@@ -20,19 +18,51 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
-
+    try:
+        bakeries = Bakery.query.all()
+        bakeries_list = [bakery.to_dict() for bakery in bakeries]
+        response = jsonify(bakeries_list)
+        return response, 200
+    except Exception as e:
+        error_message = "An error occurred while fetching bakeries."
+        print(f"Error: {e}")
+        response = jsonify({"error": error_message})
+        response.status_code = 500
+        return response
+    
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    if bakery is None:
+        return jsonify({'message': 'Bakery not found'}), 404
+    
+    bakery_dict = bakery.to_dict()
+    response = make_response(jsonify(bakery_dict), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_list = []
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    for baked_good in baked_goods_by_price:
+        bakery_dict = baked_good.to_dict()
+        baked_goods_list.append(bakery_dict)
+
+    response = make_response(jsonify(baked_goods_list), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_baked_good = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    if most_expensive_baked_good is None:
+        return jsonify({'message': 'No baked goods found'}), 404
+
+    bakery_dict = most_expensive_baked_good.to_dict()
+    response = make_response(jsonify(bakery_dict), 200)
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,10 +11,30 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
-    id = db.Column(db.Integer, primary_key=True)
+    serialize_rule = ('-bakedgoods.bakery',)
 
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+
+    def __repr__(self):
+        return f'{self.name} {self.created_at} {self.updated_at}'
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rule = ('-bakery.bakedgoods',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default = db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate = db.func.now())
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+
+    bakery = db.relationship('Bakery', backref = 'bakedgoods')
+
+    def __repr__(self):
+        return f'{self.name} {self.price} {self.created_at} {self.updated_at}'
+
     
